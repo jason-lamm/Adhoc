5f0c577aab07b3a1b7b73c8c5f92f3d9fdb0fa9a
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-14 15:53:48 -0400

    Update SharePoint_CreateStatisticsDB.sql

--- SharePoint_CreateStatisticsDB.sql
+++ SharePoint_CreateStatisticsDB.sql
@@ -1,48 +1,54 @@
 Use Internal_SharePoint
 GO
-Create Table UsageAndHealth_Statistics_Total_Unique_Users
-(
+Create Table UsageAndHealth_Statistics_Total_Unique_Users_Weekly (
 
 share varchar(max), 
 cmsintranet varchar(max), 
 capms varchar(max) 
-
 )
 
 
 
 Use Internal_SharePoint
 GO
-Create Table UsageAndHealth_Statistics_Daily_Unique_Users
-(
+Create Table UsageAndHealth_Statistics_Daily_Unique_Users (
 
 ServerUrl varchar(max),
-Day varchar(max),
+"Day" varchar(max),
 "Day of Week" varchar(max),
 "Unique Users" varchar(max)
-
 )
---DROP Table UsageAndHealth_Statistics_Daily_Unique_Users
 
 
 
 Use Internal_SharePoint
 GO
-Create Table UsageAndHealth_Statistics_TopDay_Unique_Users
-(
+Create Table UsageAndHealth_Statistics_PeakUsage_DateTime (
 
 Day varchar(max),
 UniqueUsers varchar(max)
-
 )
 
 
 
-SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
-WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
+
+Use Internal_SharePoint
+GO
+Create Table UsageAndHealth_Statistics_Top_Five_Site_URL (
+
+WebapplicationUrl varchar(max),
+SiteCollectionUrl varchar(max),
+WebSiteURL varchar(max),
+Users varchar(max)
 )
-GROUP BY FORMAT(logtime,'MM-dd hh tt')
-ORDER BY UniqueUsers DESC
+
+/*
+TABLE CLEANUP
+
+DROP TABLE UsageAndHealth_Statistics_Total_Unique_Users_Weekly
+DROP TABLE UsageAndHealth_Statistics_Daily_Unique_Users
+DROP TABLE UsageAndHealth_Statistics_PeakUsage_DateTime
+DROP TABLE UsageAndHealth_Statistics_Top_Five_Site_URL
+*/
+
+
