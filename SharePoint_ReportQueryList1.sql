5b2375f6a162352604be0c91201153f85235435f
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-15 13:36:28 -0400

    Update SharePoint_ReportQueryList1.sql

--- SharePoint_ReportQueryList1.sql
+++ SharePoint_ReportQueryList1.sql
@@ -8,14 +8,29 @@ Backend T-SQL
 
 --------------------------TOTAL UNIQUE USERS-------------------------------------------------------------------------
 --Part 1
+Use Internal_SharePoint
+GO
+INSERT INTO UsageAndHealth_Statistics_Total_Unique_Users_Weekly (ServerUrl, "Total Unique Users")
+
+SELECT ServerURL, count(distinct UserLogin) as "Total Unique Users"
+FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
+WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
+(
+	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
+)
+GROUP BY ServerURL
+
+/*
+--ORIGINAL for above
 SELECT count(distinct UserLogin) as "Total Unique Users"
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
-
-
+*/
+Use Internal_SharePoint
+GO
 SELECT count(distinct UserLogin) as "Total Unique Users"
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
@@ -23,7 +38,8 @@ WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%c
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
 
-
+Use Internal_SharePoint
+GO
 SELECT count(distinct UserLogin) as "Total Unique Users"
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
@@ -39,6 +55,8 @@ WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%'  a
 
 --------------------------DAILY UNIQUE USERS-------------------------------------------------------------------------
 --Part 2
+Use Internal_SharePoint
+GO
 SELECT FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -49,6 +67,8 @@ GROUP BY FORMAT(logtime,'MM-dd'), DATENAME(WEEKDAY,logtime)
 ORDER BY FORMAT(logtime,'MM-dd')
 
 
+Use Internal_SharePoint
+GO
 SELECT FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -59,6 +79,8 @@ GROUP BY FORMAT(logtime,'MM-dd'), DATENAME(WEEKDAY,logtime)
 ORDER BY FORMAT(logtime,'MM-dd')
 
 
+Use Internal_SharePoint
+GO
 SELECT FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -77,6 +99,8 @@ ORDER BY FORMAT(logtime,'MM-dd')
 
 ----------------------------Peak Usage Date/Time--------------------------------------------------------------------
 --Part 3
+Use Internal_SharePoint
+GO
 SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -87,6 +111,8 @@ GROUP BY FORMAT(logtime,'MM-dd hh tt')
 ORDER BY UniqueUsers DESC
 
 
+Use Internal_SharePoint
+GO
 SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -97,6 +123,8 @@ GROUP BY FORMAT(logtime,'MM-dd hh tt')
 ORDER BY UniqueUsers DESC
 
 
+Use Internal_SharePoint
+GO
 SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -115,6 +143,8 @@ ORDER BY UniqueUsers DESC
 
 --------------------------------Top Five Site URL----------------------------------------------------------------------
 --Part 4
+Use Internal_SharePoint
+GO
 SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
@@ -125,6 +155,8 @@ GROUP BY ServerUrl,SiteUrl,WebUrl
 ORDER BY Users desc
 
 
+Use Internal_SharePoint
+GO
 SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
@@ -135,6 +167,8 @@ GROUP BY ServerUrl,SiteUrl,WebUrl
 ORDER BY Users desc
 
 
+Use Internal_SharePoint
+GO
 SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
