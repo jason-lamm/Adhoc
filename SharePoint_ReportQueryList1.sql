a57563a5ac9d61e7670dba8cbb2a472aef4287cf
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-15 13:14:54 -0400

    Update SharePoint_ReportQueryList1.sql

--- SharePoint_ReportQueryList1.sql
+++ SharePoint_ReportQueryList1.sql
@@ -1,4 +1,13 @@
+/*
+SharePoint_ReportQueryList1.sql
+
+Backend T-SQL
+
+6/15/2022		Jason Lamm			Initial
+*/
+
 --------------------------TOTAL UNIQUE USERS-------------------------------------------------------------------------
+--Part 1
 SELECT count(distinct UserLogin) as "Total Unique Users"
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
@@ -29,7 +38,7 @@ WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%'  a
 
 
 --------------------------DAILY UNIQUE USERS-------------------------------------------------------------------------
-
+--Part 2
 SELECT FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -67,7 +76,7 @@ ORDER BY FORMAT(logtime,'MM-dd')
 
 
 ----------------------------Peak Usage Date/Time--------------------------------------------------------------------
-
+--Part 3
 SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
@@ -105,7 +114,7 @@ ORDER BY UniqueUsers DESC
 
 
 --------------------------------Top Five Site URL----------------------------------------------------------------------
-
+--Part 4
 SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
