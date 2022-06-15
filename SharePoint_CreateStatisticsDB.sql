cc807a83a21dacac0cca069e3a36d74dc148e1c1
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-15 13:14:06 -0400

    Update SharePoint_CreateStatisticsDB.sql

--- SharePoint_CreateStatisticsDB.sql
+++ SharePoint_CreateStatisticsDB.sql
@@ -1,18 +1,32 @@
+/*
+SharePoint_CreateStatisticsDB.sql
+
+Create tables to hold SP statistics
+
+6/15/2022		Jason Lamm			Initial
+*/
+
+--------------------------TOTAL UNIQUE USERS-------------------------------------------------------------------------
+--Part 1
 Use Internal_SharePoint
 GO
 Create Table UsageAndHealth_Statistics_Total_Unique_Users_Weekly (
 
+ID INT IDENTITY(1,1),
+ServerUrl varchar(max),
 share varchar(max), 
 cmsintranet varchar(max), 
 capms varchar(max) 
 )
 
 
-
+--------------------------DAILY UNIQUE USERS-------------------------------------------------------------------------
+--Part 2
 Use Internal_SharePoint
 GO
 Create Table UsageAndHealth_Statistics_Daily_Unique_Users (
 
+ID INT IDENTITY(1,1),
 ServerUrl varchar(max),
 "Day" varchar(max),
 "Day of Week" varchar(max),
@@ -20,22 +34,28 @@ ServerUrl varchar(max),
 )
 
 
-
+----------------------------Peak Usage Date/Time--------------------------------------------------------------------
+--Part 3
 Use Internal_SharePoint
 GO
 Create Table UsageAndHealth_Statistics_PeakUsage_DateTime (
 
+ID INT IDENTITY(1,1),
+ServerUrl varchar(max),
 Day varchar(max),
 UniqueUsers varchar(max)
 )
 
 
 
-
+--------------------------------Top Five Site URL----------------------------------------------------------------------
+--Part 4
 Use Internal_SharePoint
 GO
 Create Table UsageAndHealth_Statistics_Top_Five_Site_URL (
 
+ID INT IDENTITY(1,1),
+Date date,
 WebapplicationUrl varchar(max),
 SiteCollectionUrl varchar(max),
 WebSiteURL varchar(max),
