ae071e9710e5c27b4cb9bb45725e1db408979afb
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-20 13:14:04 -0400

    Update SharePoint_ReportQueryList1.sql

--- SharePoint_ReportQueryList1.sql
+++ SharePoint_ReportQueryList1.sql
@@ -1,4 +1,5 @@
 /*
+PHASE 2
 SharePoint_ReportQueryList1.sql
 
 Backend T-SQL
@@ -8,6 +9,16 @@ Backend T-SQL
 
 --------------------------TOTAL UNIQUE USERS-------------------------------------------------------------------------
 --Part 1
+
+--Cleanup duplicates
+Use Internal_SharePoint
+GO
+DELETE FROM UsageAndHealth_Statistics_Total_Unique_Users_Weekly WHERE "Date" = 
+(
+	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
+)
+
+
 Use Internal_SharePoint
 GO
 INSERT INTO UsageAndHealth_Statistics_Total_Unique_Users_Weekly (Date, ServerUrl, "Total Unique Users")
@@ -47,7 +58,10 @@ WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%'  a
 GROUP BY ServerURL
 
 --SELECT * FROM UsageAndHealth_Statistics_Total_Unique_Users_Weekly
---Takes 2:25 to complete query
+--Takes 3:45 to complete query
+--Verified insert process
+--Verified duplicate cleanup process
+--Verified each query for results
 ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -67,7 +81,7 @@ DELETE FROM UsageAndHealth_Statistics_Daily_Unique_Users WHERE "Date Collected" 
 
 INSERT INTO UsageAndHealth_Statistics_Daily_Unique_Users ("Date Collected", ServerURL, Day, "Day of Week", "Unique Users")
 
-SELECT CONVERT(DATE,CURRENT_TIMESTAMP), ServerURL, FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
+SELECT CONVERT(DATE,CURRENT_TIMESTAMP) AS "Date Collected", ServerURL, FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
 (
@@ -106,6 +120,8 @@ ORDER BY FORMAT(logtime,'MM-dd')
 
 
 --SELECT * FROM UsageAndHealth_Statistics_Daily_Unique_Users
+--Takes 00:59 to complete query.
+--Days with 0 results do not return a line
 ---------------------------------------------------------------------------------------------------------------------
 
 
@@ -116,39 +132,56 @@ ORDER BY FORMAT(logtime,'MM-dd')
 --Part 3
 Use Internal_SharePoint
 GO
-SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
+DELETE FROM UsageAndHealth_Statistics_PeakUsage_DateTime WHERE "Date Collected" = 
+(
+	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
+)
+
+Use Internal_SharePoint
+GO
+INSERT INTO UsageAndHealth_Statistics_PeakUsage_DateTime ("Date Collected", "ServerUrl", Day, "UniqueUsers")
+
+SELECT TOP 1 CONVERT(DATE,CURRENT_TIMESTAMP) AS "Date Collected", ServerUrl, FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
+FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
-GROUP BY FORMAT(logtime,'MM-dd hh tt')
+GROUP BY FORMAT(logtime,'MM-dd hh tt'), ServerUrl
 ORDER BY UniqueUsers DESC
 
 
 Use Internal_SharePoint
 GO
-SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
-WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
+INSERT INTO UsageAndHealth_Statistics_PeakUsage_DateTime ("Date Collected", "ServerUrl", Day, "UniqueUsers")
+
+SELECT TOP 1 CONVERT(DATE,CURRENT_TIMESTAMP) AS "Date Collected", ServerUrl, FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
+FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
+WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' /*and siteurl != '/' and siteurl != '' */ and LogTime <
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
-GROUP BY FORMAT(logtime,'MM-dd hh tt')
+GROUP BY FORMAT(logtime,'MM-dd hh tt'), ServerUrl
 ORDER BY UniqueUsers DESC
 
 
 Use Internal_SharePoint
 GO
-SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] 
+INSERT INTO UsageAndHealth_Statistics_PeakUsage_DateTime ("Date Collected", "ServerUrl", Day, "UniqueUsers")
+
+SELECT TOP 1 CONVERT(DATE,CURRENT_TIMESTAMP) AS "Date Collected", ServerUrl, FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
+FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
-GROUP BY FORMAT(logtime,'MM-dd hh tt')
+GROUP BY FORMAT(logtime,'MM-dd hh tt'), ServerUrl
 ORDER BY UniqueUsers DESC
 
+/*
+SELECT * FROM UsageAndHealth_Statistics_PeakUsage_DateTime
+Takes 1:00 to complete query.
+*/
 
 ----------------------------------------------------------------------------------------------------------------------
 
@@ -160,7 +193,16 @@ ORDER BY UniqueUsers DESC
 --Part 4
 Use Internal_SharePoint
 GO
-SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
+DELETE FROM UsageAndHealth_Statistics_PeakUsage_DateTime WHERE "Date Collected" = 
+(
+	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
+)
+
+Use Internal_SharePoint
+GO
+INSERT INTO UsageAndHealth_Statistics_Top_Five_Site_URL ("Date Collected", WebapplicationUrl, SiteCollectionUrl, WebSiteURL, Users)
+
+SELECT TOP 5 CONVERT(DATE,CURRENT_TIMESTAMP) AS "Date Collected", LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
 (
@@ -172,7 +214,9 @@ ORDER BY Users desc
 
 Use Internal_SharePoint
 GO
-SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
+INSERT INTO UsageAndHealth_Statistics_Top_Five_Site_URL ("Date Collected", WebapplicationUrl, SiteCollectionUrl, WebSiteURL, Users)
+
+SELECT TOP 5 CONVERT(DATE,CURRENT_TIMESTAMP) AS "Date Collected", LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
 (
@@ -184,7 +228,9 @@ ORDER BY Users desc
 
 Use Internal_SharePoint
 GO
-SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
+INSERT INTO UsageAndHealth_Statistics_Top_Five_Site_URL ("Date Collected", WebapplicationUrl, SiteCollectionUrl, WebSiteURL, Users)
+
+SELECT TOP 5 CONVERT(DATE,CURRENT_TIMESTAMP) AS "Date Collected", LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
 FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
 WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
 (
@@ -193,3 +239,9 @@ WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' an
 GROUP BY ServerUrl,SiteUrl,WebUrl
 ORDER BY Users desc
 
+
+/*
+Query takes 00:24 to complete
+SELECT * FROM UsageAndHealth_Statistics_Top_Five_Site_URL
+*/
+--------------------------------------------------------------------------------------------
