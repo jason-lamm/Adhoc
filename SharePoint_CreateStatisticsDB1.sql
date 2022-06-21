15a19924ea1601751d6c4adcd700e85efa43e751
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-21 14:22:47 -0400

    Update SharePoint_CreateStatisticsDB1.sql

--- SharePoint_CreateStatisticsDB1.sql
+++ SharePoint_CreateStatisticsDB1.sql
@@ -29,7 +29,7 @@ Create Table UsageAndHealth_Statistics_Daily_Unique_Users (
 ID INT IDENTITY(1,1),
 "Date Collected" date,
 ServerUrl varchar(max),
-"Day" varchar(max),
+"Day" datetime,
 "Day of Week" varchar(max),
 "Unique Users" varchar(max)
 )
@@ -66,7 +66,7 @@ Users varchar(max)
 
 /*
 TABLE CLEANUP
-
+Use Internal_SharePoint
 DROP TABLE UsageAndHealth_Statistics_Total_Unique_Users_Weekly
 DROP TABLE UsageAndHealth_Statistics_Daily_Unique_Users
 DROP TABLE UsageAndHealth_Statistics_PeakUsage_DateTime
