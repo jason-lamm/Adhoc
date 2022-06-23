c0dc184996ec2cae0a033db5e513fa361ecd83d4
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-23 14:05:58 -0400

    Update RS_QuerySet1_Phase3.sql

--- RS_QuerySet1_Phase3.sql
+++ RS_QuerySet1_Phase3.sql
@@ -2,24 +2,19 @@
 PHASE 3
 ReportingServices_QuerySet1.sql
 
-Backend T-SQL
 
-6/21/2022		Jason Lamm			Initial
+
+6/17/2022		Jason Lamm			Initial
+6/23/2022		"					Implemented Fixes, cleanup
+
+
+Queries for Reporting Services
 */
 
 
 --------------------------TOTAL UNIQUE USERS-------------------------------------------------------------------------
 --Part 1
----share.cms.gov 
-/*ORIGINAL
-SELECT count(distinct UserLogin) as "Total Unique Users"
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-*/
----NEW
+
 SELECT "Total Unique Users"
 FROM UsageAndHealth_Statistics_Total_Unique_Users_Weekly with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and Date =
@@ -28,32 +23,14 @@ WHERE ServerUrl = 'https://share.cms.gov' and Date =
 )
 
 
----------------------------------------------------------------------------------------------
-/*ORIGINAL
-SELECT count(distinct UserLogin) as "Total Unique Users"
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-*/
----NEW
 SELECT "Total Unique Users"
 FROM UsageAndHealth_Statistics_Total_Unique_Users_Weekly with(nolock)
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and Date =
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
----------------------------------------------------------------------------------------------
-/*ORIGINAL
-SELECT count(distinct UserLogin) as "Total Unique Users"
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%'  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-*/
----NEW
+
+
 SELECT "Total Unique Users"
 FROM UsageAndHealth_Statistics_Total_Unique_Users_Weekly with(nolock)
 WHERE ServerUrl = 'https://capms.cms.gov' and Date =
@@ -62,6 +39,8 @@ WHERE ServerUrl = 'https://capms.cms.gov' and Date =
 )
 
 /*
+INTERNAL TESTING:
+
 TRUNCATE TABLE UsageAndHealth_Statistics_Total_Unique_Users_Weekly
 SELECT * FROM UsageAndHealth_Statistics_Total_Unique_Users_Weekly
 
@@ -71,20 +50,9 @@ SELECT * FROM UsageAndHealth_Statistics_Total_Unique_Users_Weekly
 
 
 
-
 --------------------------DAILY UNIQUE USERS-------------------------------------------------------------------------
 --Part 2
-/*ORIGINAL
-SELECT FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY FORMAT(logtime,'MM-dd'), DATENAME(WEEKDAY,logtime)
-ORDER BY FORMAT(logtime,'MM-dd')
-*/
----NEW
+
 SELECT Day, "Day of Week", "Unique Users"
 FROM UsageAndHealth_Statistics_Daily_Unique_Users with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and "Date Collected" =
@@ -93,64 +61,36 @@ WHERE ServerUrl = 'https://share.cms.gov' and "Date Collected" =
 )
 
 
---TRUNCATE TABLE UsageAndHealth_Statistics_Daily_Unique_Users 
---SELECT * FROM UsageAndHealth_Statistics_Daily_Unique_Users 
----------------------------------------------------------------------------------------------
-/*ORIGINAL
-SELECT FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' /*and siteurl != '/' and siteurl != ''*/  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY FORMAT(logtime,'MM-dd'), DATENAME(WEEKDAY,logtime)
-ORDER BY FORMAT(logtime,'MM-dd')
-*/
----NEW
 SELECT Day, "Day of Week", "Unique Users"
 FROM UsageAndHealth_Statistics_Daily_Unique_Users with(nolock)
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and "Date Collected" =
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
----------------------------------------------------------------------------------------------
-/*ORIGINAL
-SELECT FORMAT(logtime,'MM-dd') AS 'Day', DATENAME(WEEKDAY,logtime) AS 'Day of Week', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY FORMAT(logtime,'MM-dd'), DATENAME(WEEKDAY,logtime)
-ORDER BY FORMAT(logtime,'MM-dd')
-*/
----NEW
+
+
 SELECT Day, "Day of Week", "Unique Users"
 FROM UsageAndHealth_Statistics_Daily_Unique_Users with(nolock)
 WHERE ServerUrl = 'https://capms.cms.gov' and "Date Collected" =
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
+
+
+/*
+INTERNAL TESTING
+TRUNCATE TABLE UsageAndHealth_Statistics_Daily_Unique_Users 
+SELECT * FROM UsageAndHealth_Statistics_Daily_Unique_Users 
+
+*/
 ---------------------------------------------------------------------------------------------
-
-
 
 
 
 
 ----------------------------Peak Usage Date/Time--------------------------------------------------------------------
 --Part 3
-/*ORIGINAL
-SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY FORMAT(logtime,'MM-dd hh tt')
-ORDER BY UniqueUsers DESC
-*/
---NEW
+
 SELECT Day, UniqueUsers
 FROM UsageAndHealth_Statistics_PeakUsage_DateTime with(nolock)
 WHERE ServerUrl = 'https://share.cms.gov' and "Date Collected" =
@@ -158,43 +98,30 @@ WHERE ServerUrl = 'https://share.cms.gov' and "Date Collected" =
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
 
---SELECT * FROM UsageAndHealth_Statistics_PeakUsage_DateTime
----------------------------------------------------------------------------------------------
-/*ORIGINAL
-SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' /*and siteurl != '/' and siteurl != '' */ and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY FORMAT(logtime,'MM-dd hh tt')
-ORDER BY UniqueUsers DESC
-*/
---NEW
+
 SELECT Day, UniqueUsers
 FROM UsageAndHealth_Statistics_PeakUsage_DateTime with(nolock)
 WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and "Date Collected" =
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
----------------------------------------------------------------------------------------------
-/*
-SELECT TOP 1 FORMAT(logtime,'MM-dd hh tt') AS 'Day', COUNT(DISTINCT(userlogin)) AS UniqueUsers
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and siteurl != '/' and siteurl != ''  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY FORMAT(logtime,'MM-dd hh tt')
-ORDER BY UniqueUsers DESC
-*/
---NEW
+
+
 SELECT Day, UniqueUsers
 FROM UsageAndHealth_Statistics_PeakUsage_DateTime with(nolock)
 WHERE ServerUrl = 'https://capms.cms.gov' and "Date Collected" =
 (
 	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
 )
+
+
+/*
+INTERNAL TESTING
+
+TRUNCATE TABLE UsageAndHealth_Statistics_PeakUsage_DateTime
+SELECT * FROM UsageAndHealth_Statistics_PeakUsage_DateTime
+
+*/
 ---------------------------------------------------------------------------------------------
 
 
@@ -202,17 +129,7 @@ WHERE ServerUrl = 'https://capms.cms.gov' and "Date Collected" =
 
 --------------------------------Top Five Site URL----------------------------------------------------------------------
 --Part 4
-/*ORIGINAL
-SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://share.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY ServerUrl,SiteUrl,WebUrl
-ORDER BY Users desc
-*/
---NEW
+
 SELECT WebapplicationUrl, SiteCollectionURL, WebSiteURL, Users
 FROM UsageAndHealth_Statistics_Top_Five_Site_URL with(nolock)
 WHERE WebapplicationUrl = 'https://share.cms.gov' and "Date Collected" =
@@ -222,19 +139,8 @@ WHERE WebapplicationUrl = 'https://share.cms.gov' and "Date Collected" =
 GROUP BY WebapplicationUrl, SiteCollectionURL, WebSiteURL, Users
 ORDER BY Users desc
 
---SELECT * FROM UsageAndHealth_Statistics_Top_Five_Site_URL
----------------------------------------------------------------------------------------------
-/*
-SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://cmsintranet.share.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY ServerUrl,SiteUrl,WebUrl
-ORDER BY Users desc
-*/
---NEW
+
+
 SELECT WebapplicationUrl, SiteCollectionURL, WebSiteURL, Users
 FROM UsageAndHealth_Statistics_Top_Five_Site_URL with(nolock)
 WHERE WebapplicationUrl = 'https://cmsintranet.share.cms.gov' and "Date Collected" =
@@ -243,19 +149,9 @@ WHERE WebapplicationUrl = 'https://cmsintranet.share.cms.gov' and "Date Collecte
 )
 GROUP BY WebapplicationUrl, SiteCollectionURL, WebSiteURL, Users
 ORDER BY Users desc
----------------------------------------------------------------------------------------------
-/*
-ORIGINAL
-SELECT TOP 5 LOWER(ServerUrl) as WebapplicationUrl, LOWER(SiteUrl) as SiteCollectionURL, LOWER(WebUrl) as WebSiteURL, count(distinct UserLogin) as Users
-FROM [SP_UsageAndHealth].[dbo].[RequestUsage] with(nolock)
-WHERE ServerUrl = 'https://capms.cms.gov' and UserLogin not like '%cms\svc-%' and WebUrl != ''  and WebUrl != '/'  and LogTime <
-(
-	SELECT CONVERT(DATE,CURRENT_TIMESTAMP)
-)
-GROUP BY ServerUrl,SiteUrl,WebUrl
-ORDER BY Users desc
-*/
---NEW
+
+
+
 SELECT WebapplicationUrl, SiteCollectionURL, WebSiteURL, Users
 FROM UsageAndHealth_Statistics_Top_Five_Site_URL with(nolock)
 WHERE WebapplicationUrl = 'https://capms.cms.gov' and "Date Collected" =
@@ -264,3 +160,12 @@ WHERE WebapplicationUrl = 'https://capms.cms.gov' and "Date Collected" =
 )
 GROUP BY WebapplicationUrl, SiteCollectionURL, WebSiteURL, Users
 ORDER BY Users desc
+
+
+/*
+INTERNAL TESTING
+
+SELECT * FROM UsageAndHealth_Statistics_Top_Five_Site_URL
+TRUNCATE TABLE UsageAndHealth_Statistics_Top_Five_Site_URL
+
+*/
